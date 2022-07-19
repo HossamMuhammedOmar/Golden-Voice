ba1490c9627c0d4f428269f5080fd90b3dd81cf5
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-07-19 06:02:29 +0200

    add fontawesome component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Player() {
   return (
     <div className="player-container">
@@ -6,7 +13,15 @@ export default function Player() {
         <input type="range" />
         <p>end time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
     </div>
   );
 }
