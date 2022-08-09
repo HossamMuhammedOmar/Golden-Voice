9cff865de73c5cebef1471aa703eecb831dfba79
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-08-09 07:12:35 +0200

    use Ref

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Song activeQuran={currentQuran} />
-      <Player />
+      <Player activeQuran={currentQuran} />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-export default function Player() {
+export default function Player({ activeQuran }) {
+  // Ref
+  const audioRef = useRef(null);
+
+  // Event Handler
+  const playHandler = () => {
+    console.log(audioRef.current);
+  };
+
   return (
     <div className="player-container">
       <div className="time-control">
@@ -15,13 +24,19 @@ export default function Player() {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={activeQuran.audio}></audio>
     </div>
   );
 }
